6028ea01df5c505645530bc37ddbebe6257f4164
lankydude016 <43299433+lankydude016@users.noreply.github.com>
2018-09-16 05:21:46 +0800

    merging

---
+++ public/account/controllers/account.client.controller.js
@@ -0,0 +1,20 @@
+(function () {
+	"use strict";
+	angular.module("main").controller("AccountClientController", AccountClientController);
+
+	AccountClientController.$inject = ["$scope", "$state", "$interval", "$uibModal", "mainSocket", "$sce", "$rootScope"];
+
+	function AccountClientController($scope, $state, $interval, $uibModal, mainSocket, $sce, $rootScope) {
+
+		var vm = this;
+		vm.auth = angular.copy(window.user);
+		vm.deposit = 0;
+
+		vm.depositAmount = function () {
+			mainSocket.emit('wallet.update', { $inc: { amount: vm.deposit } }, function (res) {
+			});
+			/*mainSocket.emit('transaction.create', { $inc: { amount: vm.deposit } }, function (res) {
+			});*/
+		};
+	}
+})();
